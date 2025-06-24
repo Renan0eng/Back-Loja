dd941781f93ea94e40cf67114d5850d09a74bdc7
Renan Nardi <renan.nardi.dev@gmail.com>
2025-06-24 20:15:05 -0300

    feat: start auth routes and user routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
